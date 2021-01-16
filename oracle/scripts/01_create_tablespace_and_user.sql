4f4522c149e1e0afd3cb1fe8074352314fd62ee6
Andrew Bobrow <verydrybeaver@gmail.com>
2021-01-16 14:33:35 +0200

    Update 01_create_tablespace_and_user.sql

--- oracle/scripts/01_create_tablespace_and_user.sql
+++ oracle/scripts/01_create_tablespace_and_user.sql
@@ -17,13 +17,12 @@ ALTER PROFILE DEFAULT LIMIT PASSWORD_LIFE_TIME  UNLIMITED;
 -- **************************************************
 -- create the default tablespace for all applications
 -- **************************************************
+ALTER SESSION SET container = orcl;
 CREATE TABLESPACE ANDERB_TBS DATAFILE '/opt/oracle/oradata/ORCL/anderb_tbs.dbf' SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
 
 -- *************************************************
 -- create users
 -- *************************************************
-
-alter session set "_ORACLE_SCRIPT" = true;
 CREATE USER ANDERB IDENTIFIED BY password DEFAULT TABLESPACE ANDERB_TBS QUOTA UNLIMITED ON ANDERB_TBS;
 GRANT DBA TO ANDERB;
 
